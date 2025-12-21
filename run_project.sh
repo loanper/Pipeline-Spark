54357f674ade31f2771c0279386e2d688d9619f1
Loan <perrardloan@gmail.com>
2025-12-21 18:13:14 +0100

    add comments + output Analysis 1

--- run_project.sh
+++ run_project.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
 
-echo "Démarrage du pipeline US Accidents..."
-
+echo "Start of Pipeline US Accident"
 
 docker compose down
 docker compose up -d
 
-echo "Attente du démarrage du cluster Spark (10s)..."
-sleep 10
+echo "Wait of spark cluster being launch (15s)..."
+sleep 15
 
-# 3. Permission to user
-sudo chmod -R 777 src/
+sudo chmod -R 777 src/ 
 
-# 4. Exécution du Job Spark avec redirection totale d'Ivy
-echo "📊 Lancement du traitement Spark (S3 & Permissions)..."
+echo "Launch of Spark..."
 docker exec spark-master /opt/spark/bin/spark-submit \
   --conf "spark.driver.extraJavaOptions=-Divy.cache.dir=/tmp/.ivy2/cache" \
   --conf "spark.executor.extraJavaOptions=-Divy.cache.dir=/tmp/.ivy2/cache" \
   --conf "spark.jars.ivy=/tmp/.ivy2/jars" \
-  --packages org.apache.hadoop:hadoop-aws:3.3.4,com.amazonaws:aws-java-sdk-bundle:1.12.262 \
-  /opt/spark/work-dir/job.py
-
-echo "Terminé ! Vérifiez le dossier src/output_weather_analysis pour les résultats."
+  --packages org.apache.hadoop:hadoop-aws:3.3.4,com.amazonaws:aws-java-sdk-bundle:1.12.262 /opt/spark/work-dir/job.py
+#installation of S3 drivers and redirection of cache directory due to an error with writting permissions inside of spark
